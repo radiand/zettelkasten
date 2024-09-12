c1c466822a81485caff28e77c0fac7f18dccfab2
radiand <radiand@protonmail.com>
2024-09-13 00:43:36 +0200

    refactor: compose commit msg in separation of commit command

--- command_commit.go
+++ command_commit.go
@@ -1,11 +1,15 @@
 package main
 
+import "fmt"
+import "errors"
+import "strings"
+
 import "github.com/radiand/zettelkasten/internal/git"
 
 // CmdCommit carries required params to run command.
 type CmdCommit struct {
 	zettelkastenDir string
-	git   git.IGit
+	git             git.IGit
 }
 
 // Run performs git commit with all changes that happened in RootDir directory.
@@ -14,9 +18,80 @@ func (cmd CmdCommit) Run() error {
 	if err != nil {
 		return err
 	}
-	err = cmd.git.Commit()
+
+	statuses, err := cmd.git.Status()
+	if err != nil {
+		return errors.Join(err, errors.New("Could not obtain git status"))
+	}
+
+	aggregated := countStaged(statuses)
+	if !aggregated.any() {
+		return nil
+	}
+
+	commitMsg := composeCommitMessage(aggregated)
+
+	err = cmd.git.Commit(commitMsg)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func composeCommitMessage(changes aggregation) string {
+	changesStringified := []string{}
+	populateWith := func(value int, adjective string) {
+		if value == 0 {
+			return
+		}
+		changesStringified = append(
+			changesStringified,
+			fmt.Sprintf("%d %s", value, adjective),
+		)
+	}
+	populateWith(changes.added, "added")
+	populateWith(changes.copied, "copied")
+	populateWith(changes.deleted, "deleted")
+	populateWith(changes.modified, "modified")
+	populateWith(changes.renamed, "renamed")
+	return "auto: " + strings.Join(changesStringified, ", ")
+}
+
+type aggregation struct {
+	added    int
+	copied   int
+	deleted  int
+	modified int
+	renamed  int
+}
+
+func (self *aggregation) any() bool {
+	a := self.added != 0
+	c := self.copied != 0
+	d := self.deleted != 0
+	m := self.modified != 0
+	r := self.renamed != 0
+	if a || c || d || m || r {
+		return true
+	}
+	return false
+}
+
+func countStaged(statuses []git.FileStatus) aggregation {
+	aggr := aggregation{0, 0, 0, 0, 0}
+	for _, st := range statuses {
+		switch st.Staged {
+		case git.Added:
+			aggr.added++
+		case git.Copied:
+			aggr.copied++
+		case git.Deleted:
+			aggr.deleted++
+		case git.Modified:
+			aggr.modified++
+		case git.Renamed:
+			aggr.renamed++
+		}
+	}
+	return aggr
 }
--- internal/git/git.go
+++ internal/git/git.go
@@ -8,34 +8,11 @@ import "fmt"
 import "os/exec"
 import "strings"
 
-// FileChanges represents number of files added, deleted, etc. in current
-// staging.
-type FileChanges struct {
-	Added    int
-	Copied   int
-	Deleted  int
-	Modified int
-	Renamed  int
-}
-
-// Any returns true if at least one file has been changed.
-func (fc *FileChanges) Any() bool {
-	a := fc.Added != 0
-	c := fc.Copied != 0
-	d := fc.Deleted != 0
-	m := fc.Modified != 0
-	r := fc.Renamed != 0
-	if a || c || d || m || r {
-		return true
-	}
-	return false
-}
-
 // IGit interface provides version control functionalities with git.
 type IGit interface {
 	Add(paths ...string) error
-	Commit() error
-	Stat(staged bool) (FileChanges, error)
+	Commit(message string) error
+	Status() ([]FileStatus, error)
 }
 
 // ShellGit is a Git interface implementation based on spawning shell process.
@@ -56,38 +33,11 @@ func (instance *ShellGit) Add(paths ...string) error {
 }
 
 // Commit performs git commit with custom message.
-func (instance *ShellGit) Commit() error {
-	changes, err := instance.Stat(true)
-	if err != nil {
-		return errors.Join(
-			err,
-			errors.New("Cannot commit changes because could not obtain change list"),
-		)
-	}
-	if !changes.Any() {
-		return nil
-	}
-
-	changesStringified := []string{}
-	populateWith := func(value int, adjective string) {
-		if value == 0 {
-			return
-		}
-		changesStringified = append(
-			changesStringified,
-			fmt.Sprintf("%d %s", value, adjective),
-		)
-	}
-	populateWith(changes.Added, "added")
-	populateWith(changes.Copied, "copied")
-	populateWith(changes.Deleted, "deleted")
-	populateWith(changes.Modified, "modified")
-	populateWith(changes.Renamed, "renamed")
-	commitmsg := "auto: " + strings.Join(changesStringified, ", ")
-
-	cmd := exec.Command("git", "-C", instance.WorktreePath, "commit", "-m", commitmsg)
-
-	err = cmd.Run()
+func (instance *ShellGit) Commit(message string) error {
+	cmd := exec.Command(
+		"git", "-C", instance.WorktreePath, "commit", "-m", message,
+	)
+	err := cmd.Run()
 	if err != nil {
 		return errors.Join(
 			err,
@@ -95,44 +45,6 @@ func (instance *ShellGit) Commit() error {
 		)
 	}
 	return nil
-}
-
-// Stat obtains number of changed files.
-func (instance *ShellGit) Stat(staged bool) (FileChanges, error) {
-	args := []string{
-		"-C",
-		instance.WorktreePath,
-		"diff",
-		"--name-status",
-	}
-	if staged {
-		args = append(args, "--cached")
-	}
-	cmd := exec.Command("git", args...)
-
-	out, err := cmd.Output()
-	if err != nil {
-		return FileChanges{}, errors.Join(err, errors.New("git diff failed"))
-	}
-	changes := FileChanges{}
-	for _, line := range strings.Split(string(out), "\n") {
-		if len(line) < 1 {
-			continue
-		}
-		switch line[0] {
-		case 'A':
-			changes.Added++
-		case 'C':
-			changes.Copied++
-		case 'D':
-			changes.Deleted++
-		case 'M':
-			changes.Modified++
-		case 'R':
-			changes.Renamed++
-		}
-	}
-	return changes, nil
 }
 
 // Status obtains git statuses of all paths in working directory. Note: this is
