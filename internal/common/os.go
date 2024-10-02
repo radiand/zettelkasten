c20367207123b2ea24700969894267f9d5b29e29
radiand <radiand@protonmail.com>
2024-10-02 19:53:42 +0200

    feat: add common.Exists

--- internal/common/os.go
+++ internal/common/os.go
@@ -12,3 +12,15 @@ func ExpandHomeDir(text string) string {
 	}
 	return text
 }
+
+// Exists returns whether the given file or directory exists
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
