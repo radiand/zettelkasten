43bd72bfae3092b198fb94c84489563a58f20822
radiand <radiand@protonmail.com>
2024-10-06 15:22:45 +0200

    refactor: wrap subcommand args parsing with dedicated functions

--- main.go
+++ main.go
@@ -4,47 +4,100 @@ Zettelkasten - plain text notes with toml metadata
 package main
 
 import "flag"
-import "os"
-import "time"
 import "fmt"
+import "os"
+import "strings"
+import "time"
 
 import "github.com/radiand/zettelkasten/internal/common"
 import "github.com/radiand/zettelkasten/internal/config"
 import "github.com/radiand/zettelkasten/internal/git"
 
-func main() {
-	// Global flags.
-	flagConfigPath := flag.String(
+// COMMANDS stores help string for all subcommands.
+var COMMANDS = map[string]string{
+	"init":   "Create config and required directories.",
+	"new":    "Create new note.",
+	"link":   "Find link between notes and update headers.",
+	"get":    "Get key from config or note by UID.",
+	"commit": "Generate commit message and execute git commit.",
+	"health": "Perform health check.",
+}
+
+type globalArgs struct {
+	configPath string
+	subcommand string
+	subArgs    []string
+}
+
+type cmdCommitArgs struct {
+	cooldown time.Duration
+}
+
+type cmdGetArgs struct {
+	query string
+}
+
+type cmdNewArgs struct {
+	stdout bool
+}
+
+func flagprint(lines []string) {
+	fmt.Fprintln(
+		flag.CommandLine.Output(),
+		strings.Join(
+			lines,
+			"\n",
+		),
+	)
+}
+
+func subcommandUsage(name string, help string) {
+	flagprint([]string{
+		help,
+		"",
+		"Usage:",
+		"  zettelkasten [global-options] " + name,
+	})
+}
+
+func subcommandUsageWithOptions(flagset *flag.FlagSet, name string, help string) {
+	flagprint([]string{
+		help,
+		"",
+		"Usage:",
+		"  zettelkasten [global-options] " + name + " [options]",
+		"",
+		"Options:",
+	})
+	flagset.PrintDefaults()
+}
+
+func parseGlobalArgs() globalArgs {
+	configPath := flag.String(
 		"config",
 		"~/.config/zettelkasten/config.toml",
 		"Path to config.toml file",
 	)
+	flag.Usage = func() {
+		flagprint([]string{
+			"Usage:",
+			"  zettelkasten [options] <command>",
+			"",
+			"Options:",
+		})
+		flag.PrintDefaults()
 
-	// 'new' cmd flags.
-	cmdNew := flag.NewFlagSet("new", flag.ExitOnError)
-	var cmdNewFlagStdout bool
-	cmdNew.BoolVar(
-		&cmdNewFlagStdout,
-		"stdout",
-		false,
-		"If true, print new note to stdout, otherwise save to file.",
-	)
-
-	cmdCommit := flag.NewFlagSet("commit", flag.ExitOnError)
-	var cmdCommitFlagCooldown string
-	cmdCommit.StringVar(
-		&cmdCommitFlagCooldown,
-		"cooldown",
-		"0s",
-		"Setup how much time has to pass to allow commiting a file.",
-	)
-
-	cmdGet := flag.NewFlagSet("get", flag.ExitOnError)
-
-	// Parse global flags.
+		commandsLines := []string{
+			"",
+			"Available commands:",
+		}
+		for cmdName, cmdHelp := range COMMANDS {
+			commandsLines = append(commandsLines, fmt.Sprintf("  %-10s %s", cmdName, cmdHelp))
+		}
+		flagprint(commandsLines)
+	}
 	flag.Parse()
 
-	// Parse subcommand flags.
 	args := flag.Args()
 	if len(args) == 0 {
 		fmt.Fprintln(os.Stderr, "Please specify a subcommand. Available: new, health, link, commit.")
@@ -52,16 +105,86 @@ func main() {
 	}
 	cmd, args := args[0], args[1:]
 
-	if cmd == "init" {
+	return globalArgs{configPath: *configPath, subcommand: cmd, subArgs: args}
+}
+
+func parseCmdNew (args []string) cmdNewArgs {
+	flagset := flag.NewFlagSet("new", flag.ExitOnError)
+	stdout := flagset.Bool(
+		"stdout",
+		false,
+		"If true, print new note to stdout, otherwise save to file.",
+	)
+	flagset.Usage = func() { subcommandUsageWithOptions(flagset, "new", COMMANDS["new"]) }
+	err := flagset.Parse(args)
+	try(err, "Invalid arguments")
+	return cmdNewArgs{stdout: *stdout}
+}
+
+func parseCmdCommit(args []string) cmdCommitArgs {
+	flagset := flag.NewFlagSet("commit", flag.ExitOnError)
+	cooldown := flagset.Duration(
+		"cooldown",
+		time.Duration(0),
+		"Setup how much time has to pass to allow commiting a file.",
+	)
+	flagset.Usage = func() { subcommandUsageWithOptions(flagset, "commit", COMMANDS["commit"]) }
+	err := flagset.Parse(args)
+	try(err, "Invalid arguments")
+	return cmdCommitArgs{cooldown: *cooldown}
+}
+
+func parseCmdGet(args []string) cmdGetArgs {
+	flagset := flag.NewFlagSet("get", flag.ExitOnError)
+	flagset.Usage = func() { subcommandUsage("get", COMMANDS["get"]) }
+	err := flagset.Parse(args)
+	try(err, "Invalid arguments")
+
+	hint := "Provide key from config or note UID."
+	if flagset.NArg() < 1 {
+		fmt.Fprintf(os.Stderr, "Argument required. %s\n", hint)
+		os.Exit(1)
+	}
+	if flagset.NArg() > 1 {
+		fmt.Fprintf(os.Stderr, "Too many arguments. %s\n", hint)
+		os.Exit(1)
+	}
+
+	return cmdGetArgs{query: flagset.Arg(0)}
+}
+
+func parseCmdInit(args []string) {
+	flagset := flag.NewFlagSet("init", flag.ExitOnError)
+	flagset.Usage = func() { subcommandUsage("init", COMMANDS["init"]) }
+	flagset.Parse(args)
+}
+
+func parseCmdLink(args []string) {
+	flagset := flag.NewFlagSet("link", flag.ExitOnError)
+	flagset.Usage = func() { subcommandUsage("link", COMMANDS["link"]) }
+	flagset.Parse(args)
+}
+
+func parseCmdHealth(args []string) {
+	flagset := flag.NewFlagSet("health", flag.ExitOnError)
+	flagset.Usage = func() { subcommandUsage("health", COMMANDS["health"]) }
+	flagset.Parse(args)
+}
+
+func main() {
+	globalArgs := parseGlobalArgs()
+
+	if globalArgs.subcommand == "init" {
+		parseCmdInit(globalArgs.subArgs)
 		cmdInitRunner := CmdInit{
-			configPath: *flagConfigPath,
+			configPath: globalArgs.configPath,
 			notesDir:   "~/vault/zettelkasten/notes",
 		}
 		try(cmdInitRunner.Run(), "Command failed.")
 		os.Exit(0)
 	}
 
-	config, err := config.GetConfigFromFile(common.ExpandHomeDir(*flagConfigPath))
+	config, err := config.GetConfigFromFile(common.ExpandHomeDir(globalArgs.configPath))
 	try(err, "Cannot get config.")
 
 	zettelkastenDir := common.ExpandHomeDir(config.ZettelkastenDir)
@@ -69,56 +192,45 @@ func main() {
 		return &git.ShellGit{WorktreePath: workdir}
 	}
 
-	switch cmd {
+	switch globalArgs.subcommand {
 	case "new":
-		cmdNew.Parse(args)
+		parsedArgs := parseCmdNew(globalArgs.subArgs)
 		cmdNewRunner := CmdNew{
 			zettelkastenDir: zettelkastenDir,
-			stdout:          cmdNewFlagStdout,
+			stdout:          parsedArgs.stdout,
 		}
 		try(cmdNewRunner.Run(), "Command failed.")
 	case "health":
+		parseCmdHealth(globalArgs.subArgs)
 		cmdHealthRunner := CmdHealth{
 			zettelkastenDir: zettelkastenDir,
 		}
 		try(cmdHealthRunner.Run(), "Command failed.")
 	case "link":
+		parseCmdLink(globalArgs.subArgs)
 		cmdLinkRunner := CmdLink{
 			zettelkastenDir: zettelkastenDir,
 		}
 		try(cmdLinkRunner.Run(), "Command failed.")
 	case "commit":
-		cmdCommit.Parse(args)
-		cooldown, err := time.ParseDuration(cmdCommitFlagCooldown)
-		try(err, "Invalid cooldown.")
+		parsedArgs := parseCmdCommit(globalArgs.subArgs)
 		cmdCommitRunner := CmdCommit{
 			dirs:       []string{zettelkastenDir},
 			gitFactory: gitFactory,
 			nowtime:    common.Now,
 			modtime:    common.ModificationTime,
-			cooldown:   cooldown,
+			cooldown:   parsedArgs.cooldown,
 		}
 		try(cmdCommitRunner.Run(), "Command failed.")
 	case "get":
-		cmdGet.Parse(args)
-
-		hint := "Provide key from config or note UID."
-		if cmdGet.NArg() < 1 {
-			fmt.Fprintf(os.Stderr, "Argument required. %s\n", hint)
-			os.Exit(1)
-		}
-		if cmdGet.NArg() > 1 {
-			fmt.Fprintf(os.Stderr, "Too many arguments. %s\n", hint)
-			os.Exit(1)
-		}
-
+		parsedArgs := parseCmdGet(globalArgs.subArgs)
 		cmdGetRunner := CmdGet{
-			configPath: *flagConfigPath,
-			query:      cmdGet.Arg(0),
+			configPath: globalArgs.configPath,
+			query:      parsedArgs.query,
 		}
 		try(cmdGetRunner.Run(), "Command failed.")
 	default:
-		fmt.Fprintf(os.Stderr, "Unsupported command: '%s'\n", cmd)
+		fmt.Fprintf(os.Stderr, "Unsupported command: '%s'\n", globalArgs.subcommand)
 		os.Exit(1)
 	}
 }
