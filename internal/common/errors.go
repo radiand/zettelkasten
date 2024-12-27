eff5fb582ed8c4f25c9f6de519f616593cf1960a
radiand <radiand@protonmail.com>
2024-12-27 21:58:55 +0100

    feat: better error unwrapping

--- internal/common/errors.go
+++ internal/common/errors.go
@@ -2,18 +2,45 @@ package common
 
 import "fmt"
 
+type disjoinable interface {
+	Unwrap() []error
+}
+
+type unwrappable interface {
+	Unwrap() error
+}
+
+// Disjoin recursively unwraps joined errors.
+func Disjoin(e error) []error {
+	if joined, ok := e.(disjoinable); ok {
+		var unwrapped []error
+		for _, err := range joined.Unwrap() {
+			unwrapped = append(unwrapped, Disjoin(err)...)
+		}
+		return unwrapped
+	}
+	return []error{e}
+}
+
+// LastError extracts last error from joined one.
+func LastError(e error) error {
+	if joined, ok := e.(disjoinable); ok {
+		if len(joined.Unwrap()) > 0 {
+			return LastError(joined.Unwrap()[len(joined.Unwrap())-1])
+		}
+	}
+	return e
+}
+
 // FmtErrors pretty prints errors that were wrapped by errors.Join.
-func FmtErrors(wrapped error) string {
-	if errs, ok := wrapped.(unwrappable); ok {
+func FmtErrors(e error) string {
+	disjoint := Disjoin(e)
+	if len(disjoint) > 1 {
 		formatted := "Errors in order from innermost:"
-		for idx, err := range errs.Unwrap() {
+		for idx, err := range disjoint {
 			formatted = formatted + fmt.Sprintf("\n  %d. (%T) %s", idx+1, err, err.Error())
 		}
 		return formatted
 	}
-	return wrapped.Error()
-}
-
-type unwrappable interface {
-	Unwrap() []error
+	return e.Error()
 }
